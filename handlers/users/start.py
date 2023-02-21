af51d3ddda6310b1e0efa08717f840ede6b0b939
Acer <m9602029@gmail.com>
2023-02-21 17:51:53 +0300

    o'zgartirildi

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -9,6 +9,11 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
+    await message.answer(f"Assalomu Alaykum hurmatli foydalanuvchi, {message.from_user.full_name}!",reply_markup=tillar_buttons)
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!",reply_markup=menu_buttons)
 
 
